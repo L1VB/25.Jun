5a375fd8d8f4aa45f37c0305266f6637ea8c3853
Liv B <liv.b932@gmail.com>
2025-06-25 14:03:41 +1200

    following tutorial

--- java.js
+++ java.js
@@ -1,5 +1,8 @@
-function createGreeting(name) {
-    const greeting = `Hello, ${name}!`;
-    return greeting;
-  }
-  
+function sayHello(name) {
+    const greating = 'Hello, ${name}!';
+    return greating;
+}
+
+const helloChris = sayHello("Chris");
+
+console.log(helloChris)
